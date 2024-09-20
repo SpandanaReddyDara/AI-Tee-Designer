2a9733f4431fba0793b997f6d8f6e4056867b68b
SpandanaReddyDara <spandana2186@gmail.com>
2024-09-19 22:18:08 -0400

    shirt component

---
+++ client/src/canvas/Backdrop.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { easing } from 'maath'
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
+
+const Backdrop = () => {
+    return (
+        <AccumulativeShadows
+            position={[0, 0, -0.14]}>
+            <RandomizedLight
+                amount={4}
+
+            >
+
+            </RandomizedLight>
+        </AccumulativeShadows>
+    )
+}
+
+export default Backdrop
