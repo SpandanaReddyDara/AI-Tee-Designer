c21d64c8e70749f0d5b51f83efd493a1b93b39b8
SpandanaReddyDara <spandana2186@gmail.com>
2024-09-24 22:04:10 -0400

    added diffusion api

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 import express from "express";
-import * as dotenv from "dotenv";
 import cors from "cors";
+import sdiffusionRoutes from "./routes/sdiffusion.routes.js";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: "50MB" }));
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Hello from Spandana",
-  });
-});
 
-app.listen(8080, () => {
-  console.log("Server has started on 8080");
-});
+app.use(
+  cors({
+    origin: ["https://t-customizer.vercel.app"],
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+app.options("*", cors());
+app.use(express.json({ limit: "50mb" }));
+app.use("/api/sdiffusion", sdiffusionRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server has started on port ${PORT}"));
---
+++ server/routes/sdiffusion.routes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import axios from "axios";
+import fs from "fs";
+
+dotenv.config();
+const router = express.Router();
+const STABLE_DIFFUSION_API_KEY = process.env.STABLE_DIFFUSION_API_KEY;
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from Stable Diffusion route" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4",
+      {
+        headers: {
+          Authorization: `Bearer hf_${STABLE_DIFFUSION_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(req.body.inputs),
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch from Stable Diffusion API");
+    }
+    const result = await (await response.blob()).arrayBuffer();
+    res.status(200).send(Buffer.from(result));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+export default router;
